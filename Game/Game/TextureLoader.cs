8e65111b047a6ec359983434d62435ced0a0ca9f
gonleef <gonleef@tuta.io>
2017-03-13 03:06:20 +0500

    новый файл:    Game/Game/TextureLoader.cs

---
+++ Game/Game/TextureLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace MG
+{
+	static class TextureLoader
+	{
+		public static Texture2D Player { get; private set; }
+		public static Texture2D Background { get; private set; }
+
+		public static void LoadContent(ContentManager content)
+		{
+			Background = content.Load<Texture2D>("background");
+			Player = content.Load<Texture2D>("player");
+		}
+
+	}
+}
